1ea3037eb45018da95495481681b926f73ef13ee
Mihail <153065029+zuravlevmihail@users.noreply.github.com>
2023-12-20 18:33:41 +0100

    Update CollageBuilderView.swift

--- CollageBuilderView.swift
+++ CollageBuilderView.swift
@@ -17,3 +17,68 @@ struct CollageBuilderView: View {
                         size: collageSize
                     )
                 }
+                .offset(
+                    x: store.state.collageSettings.translation.x * collageSize.width,
+                    y: store.state.collageSettings.translation.y * collageSize.height
+                )
+                .scaleEffect(store.state.collageSettings.scale)
+                .layoutPriority(-1)
+            
+            VStack {
+                TopBarView()
+                DependentPointsConectorView()
+                Spacer()
+                gridEditor
+                editor
+            }
+        }
+        .environmentObject(store)
+        .ignoresSafeArea(.keyboard)
+    }
+    
+    private var gridEditor: some View {
+        HStack {
+            Button {
+                store.dispatch(.switchEditMode)
+            } label: {
+                Text(store.state.editMode.rawValue)
+            }
+            Spacer()
+            Button {
+                store.dispatch(.togglePlayCollage)
+            } label: {
+                Image(systemName: store.state.isPlayingCollage
+                      ? "pause.circle"
+                      : "play.circle")
+            }
+            .font(.title2)
+            Spacer()
+            Button {
+                store.dispatch(.toggleGrid)
+            } label: {
+                Image(systemName: "squareshape.split.3x3")
+            }
+            .font(.largeTitle)
+            .foregroundColor(store.state.isShowingGrid ? .red : .blue)
+        }
+        .padding(.horizontal, 12)
+        .frame(height: 50)
+    }
+    
+    private var editor: some View {
+        VStack {
+            Text(editorDescription)
+            .font(.title2)
+            .padding()
+            List {
+                switch store.state.selectedElement {
+                case .shape:
+                    ShapeEditorView()
+                case .text:
+                    TextSelectorView()
+                case .sticker:
+                    StickerEditorView()
+                case .none:
+                    AddShapeElementView(size: collageSize)
+                    CollageEditorView()
+                }
